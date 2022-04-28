c2c07efcfb039a1c667a6e0c9870f524d04b4b6f
fygy10 <niko.timothyl@gmail.com>
2022-04-28 17:08:22 -0400

    Modifictions to the RPS game to reflect clicking on choices instead of typing

--- external.js
+++ external.js
@@ -1,35 +1,73 @@
 const play = ["rock","paper", "scissors"];
+let computerWins = 0;
+let playerWins = 0;
+let tieAmount = 0;
+
+const rockButton = document.getElementById('rock')
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const tieText = document.getElementById("result-tie");
 
 function computerPlay () {
      return play[Math.floor(Math.random()*play.length)]
 }
 
+/*rockButton.addEventListener('click', function() {
+    let playerSelection = 'rock';
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+});
+
+paperButton.addEventListener('click', function () {
+    let playerSelection = 'paper';
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+});
+
+scissorsButton.addEventListener('click', function () {
+    let playerSelection = 'scissors';
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+});
+*/
+
+function clickListener(event) {
+    const playerSelection = event.target.id;
+    const computerSelection = computerPlay();
+    playRound(playerSelection,computerSelection);
+ }
+
+rockButton.addEventListener("click", clickListener);
+paperButton.addEventListener("click", clickListener);
+scissorsButton.addEventListener("click", clickListener);
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "Tie";
+        tieWin();
     } else if (
         (playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'paper' && computerSelection == 'rock' ) || 
         (playerSelection == 'scissors' && computerSelection == 'paper')
         ) {
-        playerScore++;    
-        return "You win!";
+        playerWin();
     } else {
-        computerScore++;
-        return "You lose! The computer wins this round."
+        computerWin();
     }
       }
 
-playerScore = parseInt(0);
-computerScore = parseInt(0);
-
-function game () {
-    for (i = 0; i <= 5; i++) {
-    let playerSelection = prompt("Pick a move");
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log('Player score = ' + playerScore);
-    console.log('Computer score = ' + computerScore);
-    }
+function playerWin() {
+    ++playerWins;
+    playerScore.innerHTML = "Player Score: " + playerWins;
 }
-game ();
+
+function computerWin() {
+    ++computerWins;
+    computerScore.innerHTML = "Computer score: " + computerWins;
+}
+
+function tieWin() {
+    ++tieAmount;
+    tieText.innerHTML = "Tied rounds: " + tieAmount;
+}
